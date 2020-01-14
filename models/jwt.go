a9c81263792b7fbe045756000339617c66d849f2
Atchett <Atchett@users.noreply.github.com>
2020-01-14 14:52:52 +0000

    commit revert

--- models/jwt.go
+++
@@ -1,6 +0,0 @@
-package models
-
-// JWT - Models the token
-type JWT struct {
-	Token string `json:"token"`
-}
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// User - Models the user
-type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
